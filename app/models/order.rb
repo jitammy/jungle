10f66fd0ab656e8a7e9897aae77cedac4696b2a8
jitammy <jitammy@gmail.com>
2019-06-15 21:32:57 +0000

    add readme

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,15 @@
 class Order < ActiveRecord::Base
 
+  def first
+
+  end
+
+  def last
+    # generate sql
+    # SELECT self.table_name + '*' FROM self.table_name ORDERBY...
+    # Order.new(total_cents: 2)
+  end
+
   belongs_to :user
   has_many :line_items
 
