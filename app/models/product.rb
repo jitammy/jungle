29f63f56ef36092d0f703bfcfa58e8f5b56a795d
jitammy <jitammy@gmail.com>
2019-06-17 23:30:19 +0000

    add product test

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,12 +4,13 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
+  has_many :reviews
+
 
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
   validates :category, presence: true
 
-  has_many :reviews
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,7 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
 
 end
