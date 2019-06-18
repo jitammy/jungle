e37b460147ee704558e8bf7bd7f89c758c216657
jitammy <jitammy@gmail.com>
2019-06-18 16:16:54 +0000

    test feature spec home page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,4 +64,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  options = {js_errors: false}
+  Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
 end
